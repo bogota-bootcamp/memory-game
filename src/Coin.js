844b0edb40d22346c41709752fd95b3b00bfe0a3
Monica Escudero <monica.escudero21@gmail.com>
2019-10-31 14:04:50 -0500

    fixing rotation in coin

--- src/Coin.js
+++ src/Coin.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 class Coin extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {coinActive: true,
+		this.state = {coinActive: false,
 			currentSide: "https://i.colnect.net/f/1175/004/1-Dollar-4-th-president-James-Madison-1809-1817.jpg",
 			coins: ["https://i.colnect.net/f/1175/001/1-Dollar-1-st-president-George-Washington-1789-1797-back.jpg",
 			"https://i.colnect.net/f/1175/004/1-Dollar-4-th-president-James-Madison-1809-1817.jpg" ]
@@ -13,15 +13,18 @@ class Coin extends React.Component {
 	}
 
 	flipCoin(){
-		console.log("flip");
-		const currentState = this.state.coinActive;//copie esto de una funcion llamada toggle
-		this.setState({ coinActive: !currentState });//copie esto tambien
-		let rand = Math.floor(Math.random() * 2); //variable para numeros aletorios entre 0 y 1
-		let selectedImage = this.state.coins[rand]; //selecciono la imagen del arreglo coin
-		//ejemplo:  coin[0] --> image de Washington
-		//          coin[1] --> imagen de James Madison
-		console.log("["+rand+"]["+selectedImage+"]")
-		this.setState({currentSide: selectedImage});
+		if(! this.isfliped){
+			console.log("flip");
+			const currentState = this.state.coinActive;//copie esto de una funcion llamada toggle
+			this.setState({ coinActive: !currentState });//copie esto tambien
+			let rand = Math.floor(Math.random() * 2); //variable para numeros aletorios entre 0 y 1
+			let selectedImage = this.state.coins[rand]; //selecciono la imagen del arreglo coin
+			//ejemplo:  coin[0] --> image de Washington
+			//          coin[1] --> imagen de James Madison
+			console.log("["+rand+"]["+selectedImage+"]")
+			this.setState({currentSide: selectedImage});
+			this.isfliped = true
+		}
 	}
 
 
