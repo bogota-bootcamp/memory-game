14745f06845d9e81035dabca53c79b6e5867f238
EC2 Default User <ec2-user@ip-172-31-47-13.us-east-2.compute.internal>
2019-11-01 19:29:49 +0000

    Coin Anim and Rand

--- src/Coin.js
+++ src/Coin.js
@@ -5,29 +5,33 @@ class Coin extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {coinActive: false,
-			currentSide: "/redcoin.jpg",
-			coins: ["redcoin_back.jpg",
-			"redcoin.jpg" ],
+			currentState: null,
+			coins: ["redcoin.jpg", "redcoin_back.jpg" ],
 		} //las imagenes las puse en la class porque el constructor definen las variables, osea las imagenes y todo.
+	}
+	
+	getCoin(i) {
+		return this.state.currentState || this.state.coins[i]
 	}
 
 	flipCoin(){
 		if(! this.isfliped){
 			console.log("flip");
-			const currentState = this.state.coinActive;//copie esto de una funcion llamada toggle
+			let currentState = this.state.coinActive;//copie esto de una funcion llamada toggle
 			this.setState({ coinActive: !currentState });//copie esto tambien
 			let rand = Math.floor(Math.random() * 2); //variable para numeros aletorios entre 0 y 1
 			let selectedImage = this.state.coins[rand]; //selecciono la imagen del arreglo coin
 			//ejemplo:  coin[0] --> image de Washington
 			//          coin[1] --> imagen de James Madison
 			console.log("["+rand+"]["+selectedImage+"]")
-			this.setState({currentSide: selectedImage});
+			this.setState({currentState: selectedImage});
 			this.isfliped = true
 		}
 	}
 
 
     render(){
+    	console.log(5555)
 		return (
 				<div>
 					<div className="Start">
@@ -35,9 +39,12 @@ class Coin extends React.Component {
 			            <div className="flip-card">
 				            <div className={this.state.coinActive ? 'flip-card-inner' : 'spinner'}
 	                			 onClick={() => this.flipCoin()}>
-				              <div className="flip-card-back redcoin">
-				                <img src={this.state.currentSide} alt="" />
-				              </div>
+				            	<div className="flip-card-front redcoin">
+				            		<img src={this.getCoin(1)} alt="" />
+				            	</div>
+					            <div className="flip-card-back redcoin">
+					              <img src={this.getCoin(0)} alt="" />
+					            </div>
 				            </div>
 				            <button onClick={() => {document.getElementsByClassName("Start")[0].style.display = "none"}} className="start-button">Start</button>
 			            </div>
